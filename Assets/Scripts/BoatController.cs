58459ef5e3830e4177082470a39ef0de20419a84
rynduse <furkankodak40@gmail.com>
2020-06-08 22:59:03 +0300

    BoatSim
    BoatSim

---
+++ Assets/Scripts/BoatController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (FloatObject))]
+public class BoatController : MonoBehaviour
+{
+    public Vector3 COM;
+    public float speed = 1.0f;
+    public float movementThresold = 1.0f;
+
+    Transform m_COM;
+    float verticalInput;
+    float movementFactor;
+
+    private void Update()
+    {
+        Balance();
+        Movement();
+    }
+
+    void Balance()
+    {
+        if(!m_COM)
+        {
+            m_COM = new GameObject("COM").transform;
+            m_COM.SetParent(transform);
+        }
+
+        m_COM.position = COM;
+        GetComponent<Rigidbody>().centerOfMass = m_COM.position;
+    }
+
+    void Movement()
+    {
+        movementFactor = Mathf.Lerp(movementFactor, 0.5f, Time.deltaTime / movementThresold);
+        transform.Translate(movementFactor * speed, 0.0f, 0.0f);
+    }
+}
---
+++ Assets/Scripts/FloatObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class FloatObject : MonoBehaviour
+{
+    public float waterLevel = 0.0f;
+    public float floatTreshold = 2.0f;
+    public float waterDesity = 0.125f;
+    public float downForce = 4.0f;
+    public float BouncyTime = 5.0f;
+    Vector3 Bouncy;
+
+    float forceFactor;
+    Vector3 floatForce;
+
+    private void Start()
+    {
+        Bouncy += new Vector3(0.0f, -15.0f, 0.0f);
+        GetComponent<Rigidbody>().AddForceAtPosition(Bouncy, transform.position);
+    }
+    private void FixedUpdate()
+    {
+        forceFactor = 1.0f - ((transform.position.y - waterLevel) / floatTreshold);
+
+        if(forceFactor > 0.0f)
+        {
+            floatForce = -Physics.gravity * (forceFactor - GetComponent<Rigidbody>().velocity.y * waterDesity);
+            floatForce += new Vector3(0.0f, -downForce, 0.0f);
+            GetComponent<Rigidbody>().AddForceAtPosition(floatForce, transform.position);
+        }
+    }
+
+    private void Update()
+    {
+        BouncyTime -= Time.deltaTime;
+        if (BouncyTime < 0)
+        {
+            if (this.transform.position.y > -0.5f && this.transform.position.y < 0.05f)
+            {
+                Bouncy += new Vector3(0.0f, -15.0f, 0.0f);
+                GetComponent<Rigidbody>().AddForceAtPosition(Bouncy, transform.position);
+            }
+            BouncyTime += 2.0f;
+        }
+    }
+
+}
